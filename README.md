0b00c1ac0f2248e4192f8dbaf7d4d5639280300a
Luke Siemens <luke@lsiemens.com>
2026-09-17 20:53:49 -0600

    Add multi message chat threads to the poc

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ A graph based LLM client for chats that do not fit a linear model.
 The goal is to provide a chat client based on a directed graph where you can
 go down multiple diverging lines of questioning.
 
-For now I am basing this project on the xai models and using the xai-sdk for
+For now I am basing this project on the xAI models and using the xai-sdk for
 access to the models.
--- poc/client.py
+++ poc/client.py
@@ -1,5 +1,6 @@
 import keyring
 import xai_sdk
+from xai_sdk.chat import system, user, assistant
 
 class AuthError(Exception):
     """Authentication error!"""
@@ -13,6 +14,41 @@ class ChatClient:
 
     def __init__(self):
         self.llm_client = self._initalize_client()
+
+        messages=[system("Your are a precice, highly analytical research assistant. Provide accurare, well-sourced, and nuanced information. Prioritize clarity, depth and intellectual honesty. Be concise and to the point.")]
+        chat = self.llm_client.chat.create(model="grok-4.20-non-reasoning", messages=messages)
+        self.chat_loop(chat)
+
+
+    def chat_loop(self, chat):
+        """A streaming chat thread"""
+        total_cost_usd = 0.0
+
+        while True:
+            prompt = input("\nYou: ")
+
+            # some manual commands
+            if prompt.lower() == "exit":
+                break
+
+            if prompt.lower() == "clear keys":
+                self._clear_api_key()
+                print("Exiting chat!")
+                break
+
+            # add the user prompt to the chat history
+            chat.append(xai_sdk.chat.user(prompt))
+
+            # generate one responce
+            response = chat.sample()
+            print(f"\nGrok: {response.content}")
+
+            # maintain chat history
+            chat.append(response)
+
+            total_cost_usd += response.cost_usd or 0.0
+
+        print(f"Total cost: ${total_cost_usd:.4f}")
 
     def _save_api_key(self, api_key):
         if api_key is None:
@@ -31,9 +67,8 @@ class ChatClient:
 
         if api_key is None:
             print("The api key is not in your key ring.")
-
             while (api_key is None):
-                api_key = input("Enter your XAI api key: ").strip()
+                api_key = input("Enter your api key: ").strip()
                 if api_key == "":
                     print("  The key can not be empty.")
                     api_key = None
@@ -43,7 +78,3 @@ class ChatClient:
         return xai_sdk.Client(api_key=api_key)
 
 graph_cli = ChatClient()
-chat = graph_cli.llm_client.chat.create(model="grok-4.6")
-chat.append(xai_sdk.chat.user("Hello this is a test chat from the python xai_sdk interface. Say hello"))
-
-print(chat.sample().content)
