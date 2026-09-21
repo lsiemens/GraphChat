0589a458bc403f2618a7b51529e71a7c7ef87727
Luke Siemens <luke@lsiemens.com>
2026-09-21 01:41:40 -0600

    Finish poc HTTP server

--- poc/http_server/server.py
+++ poc/http_server/server.py
@@ -4,8 +4,11 @@ Simple HTTP server using sockets.
 following "https://www.codementor.io/@joaojonesventura/building-a-basic-http-server-from-scratch-in-python-1cedkg0842"
 """
 
+import urllib.parse
+import os.path
 import socket
 
+root = os.path.realpath("./html")
 SERVER_HOST = "0.0.0.0"
 SERVER_PORT = 8000
 
@@ -18,17 +21,47 @@ print(f"Socket server listening on port {SERVER_PORT} ...")
 while True:
   client_connection, client_address = server_socket.accept()
   request = client_connection.recv(1024).decode()
-  print(request)
 
-  body = ""
-  with open("./html/index.html", "r") as fin:
-    body = fin.read()
+  headers = request.split("\n")
+  filename = headers[0].split()[1]
+  filename = urllib.parse.unquote(filename)
 
-  response = "HTTP/1.0 200 OK\r\n" \
-             "Content-Type: text/html\r\n" \
-            f"Content-Length: {len(body.encode('utf-8'))}\r\n" \
-             "Connection: close\r\n\r\n" \
-            f"{body}"
+  if filename == "/":
+    filename = "/index.html"
+  path = os.path.realpath(os.path.join(root, filename.lstrip("/")))
+
+  is_valid_path = True
+  if (not os.path.exists(path)):
+    print(f"ERROR: path \"{path}\" does not exist!")
+    is_valid_path = False
+
+  if (not os.path.isfile(path)):
+    print(f"ERROR: path \"{path}\" is not a file!")
+    is_valid_path = False
+
+  if os.path.commonpath([root, path]) != root:
+    print(f"ERROR: path \"{path}\" is out side of \"./html\"!")
+    is_valid_path = False
+
+  response = "HTTP/1.0 404 NOT FOUND\r\n\r\n" \
+             "File Not Found"
+
+  if (is_valid_path):
+    try:
+      body = ""
+      with open(path, "r") as fin:
+        body = fin.read()
+
+      response = "HTTP/1.0 200 OK\r\n" \
+                 "Content-Type: text/html\r\n" \
+                f"Content-Length: {len(body.encode('utf-8'))}\r\n" \
+                 "Connection: close\r\n\r\n" \
+                f"{body}"
+    except:
+      print(f"Error: failed to read file!")
+
+      response = "HTTP/1.0 404 NOT FOUND\r\n\r\n" \
+                 "File Not Found"
 
   client_connection.sendall(response.encode('utf-8'))
   client_connection.close()
