cdf83772d606f789012d35b59f1ef6e5142eac6f
Luke Siemens <luke@lsiemens.com>
2026-09-23 13:07:05 -0600

    Complete the example API server and add CORS headers

--- engine_graph_chat/http_server/examples.py
+++ engine_graph_chat/http_server/examples.py
@@ -8,6 +8,9 @@ def HTML_server_core(root_target="./html"):
     root = os.path.realpath(root_target)
 
     def process_file_server(HTTP_request, HTTP_reply):
+        if HTTP_reply.status_code is not None:
+            return
+
         method, target, _ = HTTP_request.request_line
 
         if method != "GET":
@@ -23,30 +26,46 @@ def HTML_server_core(root_target="./html"):
     return process_file_server
 
 
-def backend_reflect(HTTP_request, HTTP_reply):
-    method, target, _ = HTTP_request.request_line
+def reflect_json_core(allow_origins, allow_methods, allow_headers, expose_headers, max_age):
+    CORS_options = (allow_origins, allow_methods, allow_headers, expose_headers, max_age)
+    set_CORS_headers = http_middleware.configure_CORS(*CORS_options)
 
-    match method:
-        case "GET":
-            http_middleware.set_simple_reply(200, "", "", HTTP_reply)
+    def process_backend_reflect(HTTP_request, HTTP_reply):
+        if HTTP_reply.status_code is not None:
             return
-        case "POST":
-            if len(HTTP_request.body) != 0:
-                if "content-type" in HTTP_request.headers:
-                    if HTTP_request.headers["content-type"] == "application/json":
-                        http_middleware.set_simple_reply(200, HTTP_request.body, ".json", HTTP_reply)
-                        return
-            http_middleware.set_simple_reply(400, "", "", HTTP_reply)
-            return
-        case _:
-            http_middleware.set_simple_reply(501, "", "", HTTP_reply)
-            pass
+
+        method, target, _ = HTTP_request.request_line
+        set_CORS_headers(HTTP_request, HTTP_reply)
+
+        match method:
+            case "GET":
+                http_middleware.set_simple_reply(200, "", "", HTTP_reply)
+                return
+            case "POST":
+                if len(HTTP_request.body) != 0:
+                    if "content-type" in HTTP_request.headers:
+                        if HTTP_request.headers["content-type"] == "application/json":
+                            http_middleware.set_simple_reply(200, HTTP_request.body, ".json", HTTP_reply)
+                            return
+                http_middleware.set_simple_reply(400, "", "", HTTP_reply)
+                return
+            case "DELETE":
+                http_middleware.set_simple_reply(201, "Content deleted", ".txt", HTTP_reply)
+            case _:
+                http_middleware.set_simple_reply(501, "", "", HTTP_reply)
+                pass
+    return process_backend_reflect
 
 
 if __name__ == "__main__":
-    #root = input("Enter path to site root: ")
-    #core = HTML_server_core(root)
-    core = backend_reflect
+    IS_TEST_FILE_SERVER = False
+
+    core = None
+    if IS_TEST_FILE_SERVER:
+        root = input("Enter path to site root: ")
+        core = HTML_server_core(root)
+    else:
+        core = reflect_json_core(["http://localhost:5173"], [], ["content-type"], [], 600)
     server = http_server.HTTPServer("0.0.0.0", 8000, core)
     server.start()
 
