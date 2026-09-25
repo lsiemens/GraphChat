40e114749b62b1f87f16187ca3ce9264f61f4c38
Luke Siemens <luke@lsiemens.com>
2026-09-24 18:53:38 -0600

    Add multiline input and formatting of markdown to chat_tui

--- engine_graphchat/core/README.md
+++ engine_graphchat/core/README.md
@@ -4,4 +4,4 @@ communication with xAI will be done through the python xAI SDK. The modules are:
 
 - `llm`: A wrapper for interacting with xAI.
 - `graph`: Types and structure of a directed acyclic graph (DAG).
-- `dialogue`: A module constructing a conversation with an LLM encoded as a DAG. 
+- `dialogue`: A module constructing a conversation with an LLM encoded as a DAG.
--- engine_graphchat/tui/chat_tui.py
+++ engine_graphchat/tui/chat_tui.py
@@ -2,32 +2,87 @@
 A commandline chat client
 """
 
+try:
+    import prompt_toolkit
+except ImportError:
+    prompt_toolkit = None
+    print("Warning: Could not import \"prompt_toolkit\", multiline input will not be available")
+
+try:
+    import rich.console
+    import rich.markdown
+except ImportError:
+    rich = None
+    print("Warning: Could not import \"rich\", formatting of markdown will not be available")
+
 from engine_graphchat.core.dialogue import dialogue_manager
 from engine_graphchat.core.dag import utils
 
-
 class Chat_TUI:
     _model_name = ""
+    _agent_name = "Grok"
 
     def __init__(self):
         self.DM = dialogue_manager.DialogueManager()
+        self._greeting = f"Connected to {self._agent_name}!"
+
+        if prompt_toolkit is not None:
+
+            kb = prompt_toolkit.key_binding.KeyBindings()
+
+            @kb.add("enter")
+            def _(event):
+                event.current_buffer.validate_and_handle()
+
+            @kb.add("escape", "enter")
+            def _(event):
+                event.current_buffer.insert_text('\n')
+
+            prompt_msg = prompt_toolkit.formatted_text.HTML("<ansicyan><b>You:</b></ansicyan> ")
+            self._session = prompt_toolkit.PromptSession(prompt_msg,
+                                                         multiline=True,
+                                                         key_bindings=kb)
+
+        if rich is not None:
+            self._console = rich.console.Console()
+
+        if prompt_toolkit is not None:
+            if rich is not None:
+                # format as a markdown new line
+                self._greeting += "  "
+            self._greeting += "\nUse Alt+Enter to make a new line."
+
+    def get_input(self):
+        if prompt_toolkit is not None:
+            return self._session.prompt()
+        else:
+            return input("You: ").strip()
+
+    def print(self, string):
+        if rich is not None:
+            self._console.print(rich.markdown.Markdown(string))
+        else:
+            print(string)
 
     def start(self):
-        print("Connected to chat!")
+        self.print(f"{self._greeting}\n")
 
         try:
             while True:
-                prompt = input("You: ").strip()
+                prompt = self.get_input()
 
                 if prompt == "":
                     continue
+
+                if prompt == "exit":
+                    break
 
                 prompt_data_node = utils.node_from_string(prompt,
                                                           self._model_name,
                                                           self.DM.active_id)
                 _, reply_data_node = self.DM.turn(prompt_data_node)
 
-                print(f"\nGrok: {reply_data_node.reply.content}")
+                self.print(f"\n\n---\n{self._agent_name}: {reply_data_node.reply.content}\n\n---\n")
         except KeyboardInterrupt:
             pass
 
